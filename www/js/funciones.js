6dea56514872ee707b7b620c1e0ac55f8689b7cb
ivoixe <ivonne@prismacm.com>
2019-04-29 17:20:50 +0200

    firebasePlugin

--- www/js/funciones.js
+++ www/js/funciones.js
@@ -37,9 +37,9 @@ var app = {
 
         window.FirebasePlugin.getToken(function(token) {
             // save this server-side and use it to push notifications to this device
-            console.log(token);
+            alert(token);
         }, function(error) {
-            console.error(error);
+            alert("error");
         });
 
 
