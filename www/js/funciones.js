f4a64ca90250530a7384787f2c331ec887e5918c
ivoixe <ivonne@prismacm.com>
2019-04-29 15:28:58 +0200

    Modify multiple files

--- www/js/funciones.js
+++ www/js/funciones.js
@@ -90,10 +90,26 @@ var app = {
         }
 
 
-
-
-
-
+        FCMPlugin.onTokenRefresh(function(token){
+            alert( token );
+        });
+        FCMPlugin.onNotification(
+            function(data){
+                if(data.wasTapped){
+                    //Notification was received on device tray and tapped by the user.
+                    alert( JSON.stringify(data) );
+                }else{
+                    //Notification was received in foreground. Maybe the user needs to be notified.
+                    alert( JSON.stringify(data) );
+                }
+            },
+            function(msg){
+                console.log('onNotification callback successfully registered: ' + msg);
+            },
+            function(err){
+                console.log('Error registering onNotification callback: ' + err);
+            }
+        );
 
     }
 
