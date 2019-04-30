e7e75c24d591e65c3fcaf9c6e56bd0913f72e153
ivoixe <ivonne@prismacm.com>
2019-04-30 12:41:04 +0200

    a

--- www/js/funciones.js
+++ www/js/funciones.js
@@ -1741,7 +1741,5 @@ function ver_notificacion(){
 
     if(localNotificationsArray) cordova.plugins.notification.local.update(localNotificationsArray);
 
-    cordova.plugins.autoStart.enable();
-
 
 }
