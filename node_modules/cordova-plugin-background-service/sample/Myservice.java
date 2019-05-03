cf559356d66fee504282ec31df35b13e6d5f9884
ivoixe <ivonne@prismacm.com>
2019-05-03 15:30:43 +0200

    multiple Files

---
+++ node_modules/cordova-plugin-background-service/sample/Myservice.java
@@ -0,0 +1,16 @@
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+
+public class Myservice extends BroadcastReceiver {
+ @Override
+ public void onReceive(Context context, Intent intent) {
+  if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
+   Intent pushIntent = new Intent(context, BackgroundService.class);
+   context.startService(pushIntent);
+  }
+ }
+}
