a6fc2827fdfb8296c6271ed43d90bf8ec85e772b
ivoixe <ivonne@prismacm.com>
2019-05-06 13:47:09 +0200

    icon

--- www/js/funciones.js
+++ www/js/funciones.js
@@ -1692,7 +1692,7 @@ function ver_notificacion(){
                 every: "minute",
                 wakeup: true,
                 vibrate: true,
-				smallIcon: 'file://smallIcon.png',
+				smallIcon: 'file://icon.png',
 				icon: 'file://doorOpen.png',
             });
         });
