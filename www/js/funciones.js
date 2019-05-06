3f9918acf578818884a84d6da4b5bd4eee174098
ivoixe <ivonne@prismacm.com>
2019-05-06 11:28:58 +0200

    funciones.js, icon

--- www/js/funciones.js
+++ www/js/funciones.js
@@ -1691,7 +1691,9 @@ function ver_notificacion(){
                 badge:1,
                 every: "minute",
                 wakeup: true,
-                vibrate: true
+                vibrate: true, 
+				smallIcon: 'res://icon.png',
+				icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzfXKe6Yfjr6rCtR6cMPJB8CqMAYWECDtDqH-eMnerHHuXv9egrw'
             });
         });
     }
