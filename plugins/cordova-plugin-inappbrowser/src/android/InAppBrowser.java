de7d1dd67330aa7c0dd9a7da89aa478c732b7aeb
ivoixe <ivonne@prismacm.com>
2019-05-07 14:25:19 +0200

    Update InAppBrowser.java

--- plugins/cordova-plugin-inappbrowser/src/android/InAppBrowser.java
+++ plugins/cordova-plugin-inappbrowser/src/android/InAppBrowser.java
@@ -1134,6 +1134,13 @@ public class InAppBrowser extends CordovaPlugin {
                 }
             }
 
+            if( url.startsWith("yourapp:") {
+                               Intent intent = new Intent(Intent.ACTION_VIEW);
+                               intent.setData(Uri.parse(url));
+                               cordova.getActivity().startActivity(intent);
+                               return true;
+                           }
+
             return false;
         }
 
