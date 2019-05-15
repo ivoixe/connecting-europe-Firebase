48621e651b1ee2af8b298bbe947ccff128e6654d
ivoixe <ivonne@prismacm.com>
2019-05-15 09:34:32 +0200

    commit

--- www/js/funciones.js
+++ www/js/funciones.js
@@ -1359,14 +1359,14 @@ function datos_portada(){
         ver_notificacion();
 
 
-            /*FCMPlugin.onTokenRefresh(function(token){
+            FCMPlugin.onTokenRefresh(function(token){
                   localStorage.setItem("token", token);
               });
               if (typeof FCMPlugin != 'undefined') {
                   FCMPlugin.getToken(function (token) {
                       localStorage.setItem("token", token);
                   });
-              }*/
+              }
 
 
         $('#login').remove();
