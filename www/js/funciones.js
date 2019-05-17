3eac410c3f390d5dcca0215d31fabcae65607346
ivoixe <ivonne@prismacm.com>
2019-05-17 15:15:43 +0200

    commit

--- www/js/funciones.js
+++ www/js/funciones.js
@@ -1357,16 +1357,9 @@ function datos_portada(){
 
     }else{
         ver_notificacion();
-
-
-            /*FCMPlugin.onTokenRefresh(function(token){
-                  localStorage.setItem("token", token);
-              });
-              if (typeof FCMPlugin != 'undefined') {
                   FCMPlugin.getToken(function (token) {
                       localStorage.setItem("token", token);
                   });
-              }*/
 
 
         $('#login').remove();
